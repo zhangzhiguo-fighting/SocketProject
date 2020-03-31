cac1b28b49602b7a86376763fd931b831eb29fc9
zhangzhiguo-fighting <2205502072@qq.com>
2020-04-01 07:23:16 +0800

    v0.1

---
+++ chatroom/client/client.c
@@ -0,0 +1,104 @@
+/*************************************************************************
+	> File Name: client.c
+	> Author: suyelu
+	> Mail: suyelu@haizeix.com
+	> Created Time: 日  3/29 18:16:00 2020
+ ************************************************************************/
+
+#include "../common/chatroom.h"
+#include "../common/tcp_client.h"
+#include "../common/common.h"
+#include "../common/color.h"
+
+char *conf = "./client.conf";
+int sockfd;
+
+char logfile[50] = {0};
+void logout(int signalnum) {
+    close(sockfd);
+    printf("您已退出.\n");
+    exit(1);
+}
+
+
+int main() {
+    int port;
+    struct Msg msg;
+    char ip[20] = {0};
+    port = atoi(get_value(conf, "SERVER_PORT"));
+    strcpy(ip, get_value(conf, "SERVER_IP"));
+    strcpy(logfile, get_value(conf, "LOG_FILE"));
+    
+
+    if ((sockfd = socket_connect(ip, port)) < 0) {
+        perror("socket_connect");
+        return 1;
+    }
+    strcpy(msg.from, get_value(conf, "MY_NAME"));
+    msg.flag = 2;
+    if (chat_send(msg, sockfd) < 0) {
+        return 2;
+    }
+    
+    struct RecvMsg rmsg = chat_recv(sockfd);
+    
+    if (rmsg.retval < 0) {
+        fprintf(stderr, "Error!\n");
+        return 1;
+    }
+
+    printf(GREEN"Server "NONE": %s\n", rmsg.msg.message);
+
+    if (rmsg.msg.flag == 3) {
+        close(sockfd);
+        return 1;
+    }
+
+    signal(SIGINT, logout);
+    pid_t pid;
+    if ((pid = fork()) < 0){
+        perror("fork");
+    }
+    if (pid == 0) {
+        sleep(2);
+        system("clear");
+        char c = 'a';
+        while (c != EOF) {
+            printf(L_PINK"Please Input Message:"NONE"\n");
+            scanf("%[^\n]s", msg.message);
+            c = getchar();
+            msg.flag = 0;
+            if (msg.message[0] == '@') {
+                msg.flag = 1;
+            }
+            chat_send(msg, sockfd);
+            memset(msg.message, 0, sizeof(msg.message));
+            system("clear");
+        }
+        close(sockfd);
+    } else {
+        freopen(logfile, "w", stdout);
+        while (1) {
+            rmsg = chat_recv(sockfd);
+            if (rmsg.retval < 0) {
+                printf("Error in Server!\n");
+                break;
+            }
+            if (rmsg.msg.flag == 0) {
+                printf(L_BLUE"%s"NONE": %s\n", rmsg.msg.from, rmsg.msg.message);
+            } else if (rmsg.msg.flag == 2) {
+                printf(YELLOW"通知信息: " NONE "%s\n", rmsg.msg.message);
+            } else if (rmsg.msg.flag == 1){
+                printf(L_BLUE"%s"L_GREEN"*"NONE": %s\n", rmsg.msg.from, rmsg.msg.message);
+            } else {
+                printf("Error!\n");
+            }
+                fflush(stdout);
+        }
+        wait(NULL);
+        close(sockfd);
+    }
+    return 0;
+}
+
+
---
+++ chatroom/server/server.c
@@ -0,0 +1,146 @@
+/*************************************************************************
+	> File Name: server.c
+	> Author: suyelu
+	> Mail: suyelu@haizeix.com
+	> Created Time: 日  3/29 16:12:32 2020
+ ************************************************************************/
+
+#include "../common/common.h"
+#include "../common/tcp_server.h"
+#include "../common/chatroom.h"
+#include "../common/color.h"
+
+struct User{
+    char name[20];
+    int online;
+    pthread_t tid;
+    int fd;
+};
+
+
+char *conf = "./server.conf";
+
+struct User *client;
+int sum = 0;
+
+
+void send_all(struct Msg msg) {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (client[i].online)
+            chat_send(msg, client[i].fd);
+    }
+}
+
+int check_name(char *name) {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (client[i].online && !strcmp(client[i].name, name)) 
+            return i;
+    }
+    return -1;
+}
+
+void *work(void *arg){
+    int sub = *(int *)arg;
+    int client_fd = client[sub].fd;
+    struct RecvMsg rmsg;
+    printf(GREEN"Login "NONE" : %s\n", client[sub].name);
+    while (1) {
+        rmsg = chat_recv(client_fd);
+        if (rmsg.retval < 0) {
+            printf(PINK"Logout: "NONE" %s \n", client[sub].name);
+            close(client_fd);
+            client[sub].online = 0;
+            sum--;
+            return NULL;
+        }
+
+        printf(BLUE"%s"NONE" : %s\n",rmsg.msg.from, rmsg.msg.message);
+        if (rmsg.msg.flag == 0) {
+            send_all(rmsg.msg);
+        } else if (rmsg.msg.flag == 1) {
+            if (rmsg.msg.message[0] == '@') {
+                char to[20] = {0};
+                int i = 1;
+                for (; i <= 20; i++) {
+                    if (rmsg.msg.message[i] == ' ')
+                        break;
+                }
+                strncpy(to, rmsg.msg.message + 1, i - 1);
+                int ind;
+                if ((ind = check_name(to)) < 0) {
+                    //告知不在线
+                    sprintf(rmsg.msg.message, "%s is not online.", to);
+                    rmsg.msg.flag = 2;
+                    chat_send(rmsg.msg, client_fd);
+                    continue;
+                } 
+                chat_send(rmsg.msg, client[ind].fd);
+            }
+        }
+    }
+    return NULL;
+}
+
+
+int find_sub() {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (!client[i].online) return i;
+    }
+    return -1;
+}
+
+bool check_online(char *name) {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (client[i].online && !strcmp(name, client[i].name)) {
+            printf(YELLOW"W"NONE": %s is online\n", name);
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int main() {
+    int port, server_listen, fd;
+    struct RecvMsg recvmsg;
+    struct Msg msg;
+    port = atoi(get_value(conf, "SERVER_PORT"));
+    client = (struct User *)calloc(MAX_CLIENT, sizeof(struct User));
+
+    if ((server_listen = socket_create(port)) < 0) {
+        perror("socket_create");
+        return 1;
+    }
+    while (1) { 
+        if ((fd = accept(server_listen, NULL, NULL)) < 0) {
+            perror("accept");
+            continue;
+        }
+        
+        recvmsg = chat_recv(fd);
+        if (recvmsg.retval < 0) {
+            close(fd);
+            continue;
+        }
+        if (check_online(recvmsg.msg.from)) {
+            msg.flag = 3;
+            strcpy(msg.message, "You have Already Login System!");
+            chat_send(msg, fd);
+            close(fd);
+            continue;
+            //拒绝连接：
+        } 
+        msg.flag = 2;
+        strcpy(msg.message, "Welcome to this chat room!");
+        chat_send(msg, fd);
+
+        int sub;
+        sum++;
+        sub = find_sub();
+        client[sub].online = 1;
+        client[sub].fd =fd;
+        strcpy(client[sub].name, recvmsg.msg.from);
+        pthread_create(&client[sub].tid, NULL, work, (void *)&sub);
+    }
+    return 0;
+}
